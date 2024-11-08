c6c80af547a6270ff99ebf7b35d1c067198126c7
Mikhail Fadin <fadin.edu@gmail.com>
2024-11-08 21:27:25 +0200

    Streamlit

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_db_connection():
     return psycopg2.connect(
         host="db",
-        database="mediawise",
-        user="mediawise",
-        password="mediawise"
+        database="deeptech",
+        user="deeptech",
+        password="deeptech"
     )
